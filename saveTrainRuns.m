9e57f572b52282e4cb4b680e265a96c2bc8a6b95
Neil Gallagher <nmg14@duke.edu>
2020-05-06 14:58:00 -0400

    saveTrainRuns no longer assumes checkpoints located with other files; deletes checkpoint files after saving

--- saveTrainRuns.m
+++ saveTrainRuns.m
@@ -1,23 +1,24 @@
-function saveTrainRuns(saveFile, datafile)
+function saveTrainRuns(saveFile, datafile, cpDir)
 % saves results of training runs from checkpoint files into a single file
 % with all relevant data
 % INPUTS
 %   saveFile: path to file where models will be saved
-%   dataFile: path to file where relevant data is saved
+%   datafile: path to file where relevant data is saved
+%   cpDir: path to directory where checkpoint files are saved
 
 % add .mat extension if not there
 if ~any(regexp(saveFile,'.mat$'))
     saveFile = [saveFile '.mat'];
 end
 
-% get a list of all checkpoint files corresponding to saveFile
-[pathPart,matches] = strsplit(saveFile,{'\\','/'});
-prefix = [];
-for i = 1:numel(pathPart)-1
-    prefix = [prefix pathPart{i} matches{i}];
+% add '/' to end of cpDir if not there
+if ~(cpDir(end)==filesep)
+    cpDir = [cpDir filesep];
 end
 
-cpFiles = dir([prefix 'chkpt_*' pathPart{end}]);
+% get a list of all checkpoint files corresponding to saveFile
+[pathPart,~] = strsplit(saveFile,{'\\','/'});
+cpFiles = dir([cpDir 'chkpt_*' pathPart{end}]);
 N = numel(cpFiles);
 
 % load saved data and initialize list of trained models
@@ -29,8 +30,10 @@ if ~exist('csfaModels','var')
 end
 
 % add training data from each checkpoint file to one list
+cpPath = {};
 for k = 1:N
-    trainInfo = load([prefix cpFiles(k).name]);
+    cpPath{k} = [cpDir cpFiles(k).name];
+    trainInfo = load(cpPath{k});
     
     % every checkpoint file has a sets variable. They should all be
     % the same, so we consolidate.
@@ -40,8 +43,13 @@ for k = 1:N
     trainInfo = rmfield(trainInfo,'sets');
     
     csfaModels = [csfaModels; trainInfo];
+    
 end
 
 load(datafile,'labels')
-
 save(saveFile,'csfaModels','sets','labels')
+
+% delete checkpoint files once all data is saved
+for k = 1:N
+   delete(cpPath{k})
+end

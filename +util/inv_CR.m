68bcfb1b95f7bd63d5d30a7adc0c15115209a214
Neil Gallagher <nmg14@duke.edu>
2020-09-22 14:03:31 -0400

    removed unused utility functions

--- +util/inv_CR.m
+++
@@ -1,9 +0,0 @@
-    function Z = inv_CR(M)
-    %     Complex matrix inversion via Real matrix inversion
-    %     F C Chang     01/28/15
-
-             A = real(M);
-             B = imag(M);
-             X = inv(A+B*inv(A)*B);
-             Y = inv(B+A*inv(B)*A);
-             Z = complex(X,-Y);
